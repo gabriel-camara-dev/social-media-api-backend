fce9b473f73b6d83002cc1d0bfc393a5facb61a3
Gabriel Camara <galvares439@gmail.com>
2025-09-30 21:21:34 -0300

    feat: user profile presenter

--- src/http/presenters/user-presenter.ts
+++ src/http/presenters/user-presenter.ts
@@ -1,4 +1,4 @@
-import { User, USER_ROLE } from '@prisma/client'
+import { Comment, Posts, Repost, User, USER_ROLE } from '@prisma/client'
 
 interface HTTPUser {
   id: string
@@ -7,6 +7,37 @@ interface HTTPUser {
   role: USER_ROLE
   createdAt: Date
   updatedAt: Date
+}
+
+interface HTTPUserProfile {
+  id: string
+  name: string
+  email: string
+  role: USER_ROLE
+  createdAt: Date
+  updatedAt: Date
+  posts: {
+    id: string
+    title: string
+    content: string
+    likes: number
+    published: boolean
+    createdAt: Date
+    updatedAt: Date
+  }[]
+  comments: {
+    id: string
+    content: string
+    likes: number
+    createdAt: Date
+    updatedAt: Date
+  }[]
+  reposts: {
+    id: string
+    postId?: string
+    commentId?: string
+    createdAt: Date
+  }[]
 }
 
 export class UserPresenter {
@@ -25,5 +56,48 @@ export class UserPresenter {
       createdAt: input.createdAt,
       updatedAt: input.updatedAt,
     }
+  }
+}
+
+export class UserProfilePresenter {
+  static toHTTP(
+    user: User & { posts: Posts[]; comments: Comment[]; reposts: Repost[] }
+  ): HTTPUserProfile {
+    return {
+      id: user.publicId,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      posts: user.posts.map((post) => ({
+        id: post.publicId,
+        title: post.title,
+        content: post.content,
+        likes: post.likes,
+        published: post.published,
+        createdAt: post.createdAt,
+        updatedAt: post.updatedAt,
+      })),
+      comments: user.comments.map((comment) => ({
+        id: comment.publicId,
+        likes: comment.likes,
+        content: comment.content,
+        createdAt: comment.createdAt,
+        updatedAt: comment.updatedAt,
+      })),
+      reposts: user.reposts.map((repost) => ({
+        id: repost.publicId,
+        postId: repost.postId ? repost.postId.toString() : undefined,
+        commentId: repost.commentId ? repost.commentId.toString() : undefined,
+        createdAt: repost.createdAt,
+      })),
+    }
+  }
+
+  static toHTTPList(
+    users: (User & { posts: Posts[]; comments: Comment[]; reposts: Repost[] })[]
+  ): HTTPUserProfile[] {
+    return users.map((user) => this.toHTTP(user))
   }
 }
