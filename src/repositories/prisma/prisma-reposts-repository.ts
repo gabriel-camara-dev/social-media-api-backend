d68a6c543b0bd6b7579976bb06821600bc54183f
Gabriel Camara <galvares439@gmail.com>
2025-09-30 21:34:59 -0300

    feat: reposts repositories

---
+++ src/repositories/prisma/prisma-reposts-repository.ts
@@ -0,0 +1,47 @@
+import { Prisma, User } from '@prisma/client'
+import { prisma } from '../../lib/prisma'
+import { RepostRepository } from '../reposts-repository'
+
+export class PrismaRepostRepository implements RepostRepository {
+  async delete(id: number) {
+    await prisma.user.delete({
+      where: {
+        id,
+      },
+    })
+  }
+
+  async update(id: number, data: Prisma.RepostUpdateInput) {
+    const repost = await prisma.repost.update({
+      where: { id },
+      data,
+    })
+
+    return repost
+  }
+
+  async create(data: Prisma.RepostCreateInput) {
+    const repost = await prisma.repost.create({
+      data,
+    })
+    return repost
+  }
+
+  async findById(id: number) {
+    const repost = await prisma.repost.findUnique({
+      where: {
+        id,
+      },
+    })
+    return repost
+  }
+
+  async findByPublicId(publicId: string) {
+    const repost = await prisma.repost.findUnique({
+      where: {
+        publicId,
+      },
+    })
+    return repost
+  }
+}
---
+++ src/repositories/reposts-repository.ts
@@ -0,0 +1,9 @@
+import { Repost, Prisma } from '@prisma/client'
+
+export interface RepostRepository {
+  create: (data: Prisma.RepostCreateInput) => Promise<Repost>
+  findById: (id: number) => Promise<Repost | null>
+  findByPublicId: (publicId: string) => Promise<Repost | null>
+  delete: (id: number) => Promise<void>
+  update: (id: number, data: Prisma.RepostUpdateInput) => Promise<Repost>
+}
